a0e38e54f2b5ee82d7d4bf44a9fb374e198cf9c2
Lyashh <broflovski363@gmail.com>
2019-10-14 21:18:57 +0300

    icons + fonts

--- client/src/Home.js
+++ client/src/Home.js
@@ -19,16 +19,68 @@ class Home extends React.Component {
   
     render() {
       return(
-        <Row >
-            <Col md={2}></Col>
-            <Col md={8} ref={el => this.scene = el}>
+        <Row className="m-t-80">
+            <Col md={4}>
+              <Row className="m-t-30">
+                <Col md={6}>
+                  <div className="icon-container">
+                    <div className="in-block">
+                      <p className="icon-title">Temperature</p>                      
+                    </div>
+                    <br/>
+                      <div>
+                        <img  className="float-l"src="img/icons/temperature.svg" width="90px" height="90px" alt="alt"/>
+                        <p className="float-l icon-text">22</p>
+                      </div>
+                    
+                  </div>
+                </Col>
+                <Col md={6}>
+                
+                  <div className="icon-container">
+                    <div className="in-block">
+                      <p className="icon-title">Humidity</p>
+                    </div>
+                    <br/>
+                    <div>
+                      <img className="float-l" src="img/icons/humidity1.svg" width="90px" height="90px" alt="alt"/>
+                      <p className="float-l icon-text">22</p>
+                    </div>
+                    
+                  </div>
+                </Col>
+                <Col md={6}>
+                  <div className="icon-container">
+                    <div className="in-block">
+                      <p className="icon-title">Smoke</p>
+                    </div>
+                    <br/>
+                    <div>
+                      <img className="float-l" src="img/icons/co2.svg" width="90px" height="90px" alt="alt"/>
+                      <p className="float-l icon-text">22</p>                      
+                    </div>
+                  </div>
+                </Col>
+                <Col md={6} >
+                  <div className="icon-container">
+                    <div className="in-block">
+                      <p className="icon-title">Vibration</p>
+                    </div>
+                    <br/>
+                    <div>
+                      <img className="float-l" src="img/icons/vibration.svg" width="90px" height="90px" alt="alt"/>
+                      <p className="float-l icon-text">22</p>                      
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+
+            <Col md={8} ref={el => this.scene = el} className="p-20">
                 <div className="layer" data-depth="0.20">
-                    <img src="servers.svg" width="100%" height="100%" alt="image description"/>
+                    <img src="servers.svg" width="90%" height="90%" alt="alt"/>
                 </div>
             </Col>
-            <Col md={2}></Col>
-
-           
         </Row>
       )
     }
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
+import './fonts/mont/stylesheet.css'
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
