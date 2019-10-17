6a0f36ebde948bd169ceafd3f2a4570ea3cea706
Lyashh <broflovski363@gmail.com>
2019-10-17 13:55:56 +0300

    finished parser

---
+++ build/parser/index.js
@@ -0,0 +1,32 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const serialport_1 = __importDefault(require("serialport"));
+const Readline = serialport_1.default.parsers.Readline;
+class Parser {
+    constructor() {
+        this._port = new serialport_1.default('COM7', {
+            baudRate: 9600
+        });
+        this._parser = new Readline({ delimiter: '\r\n' });
+        this._port.pipe(this._parser);
+        this._sensorsTitles = ['Temperature', 'Humidity', 'Vibration', 'Smoke'];
+    }
+    listenArduino() {
+        this._parser.on('data', (line) => {
+            this._sensorsTitles.forEach(title => {
+                if (line.includes(title)) {
+                    const arr = line.split(':');
+                    const sensor = {
+                        title,
+                        value: parseFloat(arr[1])
+                    };
+                    return sensor;
+                }
+            });
+        });
+    }
+}
+exports.default = Parser;
---
+++ parser/index.ts
@@ -0,0 +1,41 @@
+import SerialPort from 'serialport'
+const Readline = SerialPort.parsers.Readline
+
+interface Sensor {
+	title: String
+	value: Number
+}
+
+export default class Parser {
+	private _port: SerialPort
+	private _parser: any
+	private _sensorsTitles: Array<String>
+
+	constructor() {
+		this._port = new SerialPort('COM7', {
+			baudRate: 9600
+		  })
+		this._parser = new Readline({delimiter: '\r\n'})
+		this._port.pipe(this._parser)
+		this._sensorsTitles = ['Temperature', 'Humidity', 'Vibration', 'Smoke']
+	}
+
+	public listenArduino() {
+		this._parser.on('data', (line: any) => {
+			this._sensorsTitles.forEach(title => {
+				if(line.includes(title)) {
+					const arr = line.split(':')
+					const sensor: Sensor = {
+					   title,
+					   value: parseFloat(arr[1])
+					 }
+					return sensor 
+		   		}
+			})
+		  })
+	}
+}
+
+
+
+
