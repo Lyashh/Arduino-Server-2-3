899a2bdc71034e83abd1c6a7b072741992300f7c
Lyashh <broflovski363@gmail.com>
2019-11-22 13:59:56 +0300

    test

--- build/socket/index.js
+++ build/socket/index.js
@@ -1,4 +1,12 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -12,10 +20,16 @@ class Socket {
     init() {
         this._io.on('connect', (socket) => {
             console.log('Connected client ' + socket.id);
+            this.getSensors(socket);
             socket.on('disconnect', () => {
                 console.log('User disconnected');
             });
         });
+    }
+    getSensors(socket) {
+        socket.on('sendSensors', (data) => __awaiter(this, void 0, void 0, function* () {
+            console.log(data);
+        }));
     }
 }
 exports.default = Socket;
--- socket/index.ts
+++ socket/index.ts
@@ -11,19 +11,25 @@ interface sensorsData {
 export default class Socket {
     public _io: io.Server
     
-
     constructor(server: Server) {
         this._io = io(server)
-       
     }
 
     public init() {
         this._io.on('connect', (socket: any) => {
             console.log('Connected client ' + socket.id)
+            
+            this.getSensors(socket)
             socket.on('disconnect',() => {
                 console.log('User disconnected');
             })
           
+        })
+    }
+
+    private getSensors(socket: any) {
+        socket.on('sendSensors', async (data: sensorsData) => {
+            console.log(data);
         })
     }
 }
